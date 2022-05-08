8491a9f83ec9b7a146ced2cbd47dbcb560a3a795
jamescardona11 <jjcardonao@gmail.com>
2022-05-07 21:03:10 -0500

    combine images done

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -7,6 +7,7 @@ use std::convert::TryInto;
 #[derive(Debug)]
 enum ImageDataErrors {
   DifferentImageFormats,
+  BufferToSmall,
 }
 
 struct FloatingImage {
@@ -28,6 +29,14 @@ impl FloatingImage {
       
     }
   }
+  fn set_data(&mut self, data: Vec<u8>) -> Result<(), ImageDataErrors> {
+    if data.len() > self.data.capacity() {
+      return Err(ImageDataErrors::BufferToSmall);
+    }
+
+    self.data = data;
+    Ok(())
+  }
 }
 
 // enum Option {
@@ -45,8 +54,13 @@ fn main() -> Result<(), ImageDataErrors> {
   }
 
   let (image_1, image_2) = standardise_size(image_1, image_2);
-  let output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
+  let mut output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
 
+  let mut combined_data = combine_images(image_1, image_2);
+  output.set_data(combined_data)?;
+
+
+  image::save_buffer_with_format(output.name, &output.data, output.width, output.height, image::ColorType::Rgb8, image_format_1).unwrap();
   Ok(())
   
   
@@ -75,4 +89,38 @@ fn standardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicIma
   } else {
     (image_2.resize_exact(width, height, Triangle), image_1)
   }
+}
+
+fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
+  let vec_1 = image_1.to_rgba8().into_vec();
+  let vec_2 = image_2.to_rgba8().into_vec();
+  alternate_pixels(vec_1, vec_2)
+}
+
+fn alternate_pixels(vec_1: Vec<u8>, vec_2: Vec<u8>) -> Vec<u8> {
+  let mut combined_data = vec![0u8; vec_1.len()];
+
+  let mut i = 0;
+  while i < vec_1.len() {
+    if i % 8 == 0 {
+      combined_data.splice(i..=i+3, set_rgba(&vec_1, i, i+3));
+    }else {
+      combined_data.splice(i..=i+3, set_rgba(&vec_2, i, i+3));
+    }
+    i += 4;
+  }
+
+  combined_data
+}
+
+fn set_rgba(vec: &Vec<u8>, start: usize, end: usize) -> Vec<u8> {
+  let mut rgba = Vec::new();
+  for i in start..=end {
+    let val: u8 = match vec.get(i) {
+      Some(d) => *d,
+      None => panic!("Index out of bounds")
+    };
+    rgba.push(val);
+  }
+  rgba
 }
