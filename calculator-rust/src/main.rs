06da1c9ae40b3dd20d1173ed42a86b9ed242e375
jamescardona11 <jjcardonao@gmail.com>
2022-05-07 20:01:46 -0500

    calculator function:

--- calculator-rust/src/main.rs
+++ calculator-rust/src/main.rs
@@ -1,3 +1,32 @@
+use std::env::{args, Args};
+
 fn main() {
-  println!("Hello, world!");
+  let mut args: Args = args();
+
+  let first = args.nth(1).unwrap();
+  let opt = args.nth(0).unwrap().chars().next().unwrap();
+  let second = args.nth(0).unwrap();
+
+  let first_number: f32 = first.parse().unwrap();
+  let second_number = second.parse::<f32>().unwrap();
+
+  let result = operator(opt, first_number, second_number);
+  
+  println!("{:?}", result);
 }
+
+
+fn operator(operator: char, first_number: f32, second_number: f32) -> f32{
+  if operator == '+' {
+    return first_number + second_number;
+  } else if operator == '-' {
+    return first_number - second_number;
+  }else if operator == '*' {
+    return first_number * second_number;
+  }else if operator == '/' {
+    return first_number / second_number;
+  }else {
+    return 0.0;
+  }
+}
+
